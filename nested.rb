b0d5c4b9e7efc974e0babd2c631ac0b7d7ecb09d
Elvis Landi <elandi13@yahoo.com>
2020-11-06 00:29:12 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -16,7 +16,7 @@ def hopper
       }
     }
     
-    programmer_hash [grace_hopper]
+    programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -38,7 +38,7 @@ def alan_kay_is_known_for
       }
     }
 
-programmer_hash: [:alan_kay][:known_for]
+programmer_hash[:alan_kay][:known_for]
 
 end
 
@@ -59,7 +59,7 @@ def dennis_ritchies_language
       }
     }
 
-programmer_hash:[:dennis_ritchie][:languages][0]
+programmer_hash[:dennis_ritchie][:languages][0]
 
 end
 
@@ -87,9 +87,9 @@ def adding_matz
       }
     }
 
-programmer_hash:[:yukihiro_matsumoto] = {}
-programmer_hash:[:yukihiro_matsumoto][:known_for] = "Ruby"
-programmer_hash:[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
+programmer_hash[:yukihiro_matsumoto] = {}
+programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
 programmer_hash
 
 end
@@ -115,7 +115,7 @@ def changing_alan
     }
 alans_new_info = "GUI"
 
-  programmer_hash:[:alan_kay][:known_for] = alans_new_info
+  programmer_hash[:alan_kay][:known_for] = alans_new_info
   programmer_hash
 
 end
@@ -140,7 +140,7 @@ def adding_to_dennis
       }
     }
 
-programmer_hash: [:dennis_ritchie][:languages] << "Assembly"
+programmer_hash[:dennis_ritchie][:languages] << "Assembly"
 programmer_hash
 
 end
