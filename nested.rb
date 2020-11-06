c7b78e5ec3c617433927dc6c8d2d504ac8c3757b
Elvis Landi <elandi13@yahoo.com>
2020-11-06 00:24:28 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -115,7 +115,6 @@ def changing_alan
 programmer_hash:[:alan_kay][:known_for] = alans_new_info
 programmer_hash
 
-
 end
 
 def adding_to_dennis
@@ -137,5 +136,8 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
+
+programmer_hash: [:dennis_ritchie][:languages] << "Assembly"
+programmer_hash
 
 end
